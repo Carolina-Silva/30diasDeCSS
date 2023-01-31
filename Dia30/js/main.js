57097a3fea9186d18e5c800f1ab95cc24c41e8a9
Carolina-Silva <carol_paiva@outlook.com>
2023-01-30 22:01:11 -0300

    add day 30 vs 1.0

---
+++ Dia30/js/main.js
@@ -0,0 +1,43 @@
+/*menu show-------------------------------------*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', () =>{
+            nav.classList.toggle('show');
+        });
+    }
+}
+showMenu('nav-toggle', 'nav-menu');
+
+/*remove menu show-------------------------------------*/
+const navLink = document.querySelectorAll('nav__link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*active links-------------------------------------*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const srollY = window.pageXOffset
+    sections.forEach(current => {
+        const sectionHeight =current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=]' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=]' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+
+/*remove active links-------------------------------------*/
